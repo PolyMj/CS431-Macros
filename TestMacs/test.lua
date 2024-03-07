c8991d25d9840ccf94392f2799daf0d9f74408c7
PolyMj <ballarmj@sunypoly.edu>
2024-03-07 11:38:59 -0500

    Added server notes, partially worked group iteration in test.lua

--- TestMacs/test.lua
+++ TestMacs/test.lua
@@ -28,9 +28,38 @@ mq.bind('/mss', memspellset)
 mq.bind('/setspell', setspell)
 mq.bind('/cst', castspell)
 
+local iterate_group = function()
+	local size = mq.TLO.Group.GroupSize()
+	printf("%d members in my group", size)
+
+	local tank = mq.TLO.Group.MainTank()
+	if tank then
+		printf("Main Tank is %s", tank)
+	else
+		print("No Main Tank, searching for a tank...")
+		for i in range(0, size) do
+			local member = mq.TLO.Group.Member(i)
+			local class = member.Class()
+			if (class == "SHD" or class == "PAL" or class == "WAR") then
+				printf("%s is a tank", member.Name())
+				break
+			end
+		end
+	end
+
+	local assist = mq.TLO.Group.MainAssist()
+	if assist then
+		printf("Main Assist is %s", assist)
+	else
+		printf("No Main Assist")
+	end
+end
+
+mq.bind('/itg', iterate_group)
+
 
 while runscript do
 	printf("Test")
-	mq.cmd('/sit')
+	--mq.cmd('/sit')
 	mq.delay(1000)
 end
