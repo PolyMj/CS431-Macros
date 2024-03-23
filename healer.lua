2e2322d328d0841d41da2bdb8901ac5ed65963d7
PolyMj <ballarmj@sunypoly.edu>
2024-03-22 22:19:27 -0400

    Added comments

--- healer.lua
+++ healer.lua
@@ -34,7 +34,7 @@ printf("Debuff spell: %s", debuff_spell.Name())
 
 local MQ_FALSE = mq.TLO.Cast.Ready("NOT A SPELL")
 
-
+-- Targets the given target, or untargets if given nil target
 local function target(targ)
 	if targ then
 		mq.cmdf("/target \"%s\"", targ.Name())
@@ -46,6 +46,7 @@ local function target(targ)
 	end
 end
 
+-- Finds a tank in your group
 local function findTank()
 	-- Check prerequisites (is grouped)
 	if (not mq.TLO.Me.Grouped()) then
@@ -66,7 +67,7 @@ local function findTank()
 		end
 	end
 
-	-- Assign tank, or exit from macro if no tank was found
+	-- If a tank was found, print the tanks name. Otherwise, script should end
 	if (tank) then
 		printf("Chosen tank is %s (%s)", tank.Name(), tank.Class.ShortName())
 	else
@@ -75,12 +76,13 @@ local function findTank()
 	end
 end
 
+-- Checks if the target has the given buff (spell)
 local function hasBuff(spell, targ)
 	local val = targ.Buff(spell.Name()).ID()
 	return val
 end
 
-
+-- Check if a spell is ready
 local function spellReady(spell)
 	return not (mq.TLO.Cast.Ready(spell.Name()) == MQ_FALSE)
 end
@@ -88,11 +90,12 @@ end
 
 -- Cast a spell on a target, checks mana, buffstatus of target, and if cast is ready
 local function castSpell(spell, targ)
+	-- Check for nils
 	if not spell then
-		printf("Nil spell recieved")
+		printf("Spellcast Failed: Nil spell recieved")
 		return
 	elseif not targ then
-		printf("Nil target recieved")
+		printf("Spellcast Failed: Nil target recieved")
 		return
 	end
 
@@ -100,41 +103,43 @@ local function castSpell(spell, targ)
 	-- Check if has buff
 	if hasBuff(spell, targ) then
 		printf(" - Already has buff %s", spell.Name())
+	-- Check if spell is on cooldown
+	elseif not spellReady(spell) then
+		print(" - Spell not ready")
+	-- Check if enough mana
+	elseif (spell.Mana() > mq.TLO.Me.CurrentMana() + 2) then
+		printf(" - Insufficient mana (%d / %d)", mq.TLO.Me.CurrentMana(), spell.Mana())
+	-- Check if can target
+	elseif not target(targ) then
+		printf(" - No target found")
+	-- Cast spell
 	else
-		-- Check if spell is on cooldown
-		if not spellReady(spell) then
-			print(" - Spell not ready")
-		-- Check if enough mana
-		elseif (spell.Mana() > mq.TLO.Me.CurrentMana() + 2) then
-			printf(" - Insufficient mana (%d / %d)", mq.TLO.Me.CurrentMana(), spell.Mana())
-		-- Target and cast
-		elseif target(targ) then
-			mq.cmdf("/cast \"%s\"", spell.Name())
-			-- If successful, delay until spell has finished casting
-			local result = mq.TLO.Cast.Result()
-			if result == "CAST_SUCCESS" then
-				mq.delay(spell.MyCastTime.Raw())
-				printf(" - Finished cast")
-			-- If fizzled, delay and retry
-			elseif result == "CAST_FIZZLE" then
-				printf(" - Fizzled, retrying...")
-				mq.delay(spell.FizzleTime.Raw())
-				castSpell(spell, targ)
-			-- Otherwise, it's some weird result that probably doesn't matter idk
-			else
-				printf(" - Unexpected result: %s", result)
-			end
+		mq.cmdf("/cast \"%s\"", spell.Name())
+		-- If successful, delay until spell has finished casting
+		local result = mq.TLO.Cast.Result()
+		if result == "CAST_SUCCESS" then
+			mq.delay(spell.MyCastTime.Raw())
+			printf(" - Finished cast")
+		-- If fizzled, delay and retry
+		elseif result == "CAST_FIZZLE" then
+			printf(" - Fizzled, retrying...")
+			mq.delay(spell.FizzleTime.Raw())
+			castSpell(spell, targ)
+		-- Otherwise, it's some weird result that probably doesn't matter idk
+		else
+			printf(" - Unexpected result: %s", result)
 		end
 	end
 end
 
+-- Calculate distance between two spawns
 local function distance(from, to)
 	local dx = from.X() - to.X()
 	local dy = from.X() - to.X()
 	return math.sqrt(dx*dx + dy*dy)
 end
 
-
+-- Iterate throuh all extended targets
 local function iterateXTargets()
 	local cnt = mq.TLO.Me.XTarget()
 	if cnt < 1 then
@@ -152,23 +157,25 @@ local function iterateXTargets()
 			hostiles[i] = targ
 		else
 			-- Invalid target found, uh oh
-			print(i)
+			print(" - Invalid target at index %d", i)
 		end
 	end
 
-	-- Sort hostiles by distance
+	-- Sort hostiles by distance (closest to tank first)
 	table.sort(hostiles, 
 		function(a, b) 
 			return distance(a, tank) < distance(b, tank)
 		end
 	)
 
+	-- Perform checks for all hostiles, starting with those closest to the tank
 	for index, hostile in ipairs(hostiles) do
 		printf(" - Dist = %f ||  %s", distance(hostile, tank), hostile.Name())
 		-- Cast debuff
 		if mq.TLO.Me.PctMana() > DEBUFF_MANA_TH then
 			castSpell(debuff_spell, hostile)
 		end
+		-- Cast attack
 		if mq.TLO.Me.PctMana() > ATK_MANA_TH then
 			castSpell(attack_spell, hostile)
 		end
@@ -176,7 +183,7 @@ local function iterateXTargets()
 
 end
 
-
+-- Heall tank, self, and other group members
 local function healAll()
 	-- Heal tank
 	if (tank and tank.PctHPs() < TANK_HEAL_TH) then
@@ -201,7 +208,7 @@ local function healAll()
 	end
 end
 
-
+-- Operations for when the tank is in combat
 local function inCombatOps()
 	print("In combat")
 	
@@ -210,7 +217,7 @@ local function inCombatOps()
 	iterateXTargets()
 end
 
-
+-- Temporary combat check function
 local function isInCombat()
 	-- if mq.TLO.Me.TargetOfTarget.Name() then
 	-- 	return false
@@ -220,7 +227,7 @@ local function isInCombat()
 	return mq.TLO.Me.XTarget() > 0
 end
 
-
+-- Regen the healer's mana
 local function regenMana()
 	while (mq.TLO.Me.PctMana() < 99) and not isInCombat() do
 		if not mq.TLO.Me.Sitting() then mq.cmd("/sit") end
@@ -230,6 +237,7 @@ local function regenMana()
 	mq.cmd("/stand")
 end
 
+-- Operatiosn for when the tank is out of combat
 local function outCombatOps()
 	printf("Out of combat")
 	castSpell(buff_spell_1, tank)
@@ -239,7 +247,7 @@ local function outCombatOps()
 	end
 end
 
-
+-- Navigation stuff (might not be used here?)
 local function nav()
 	print("NAVIGATE")
 end
@@ -263,8 +271,8 @@ while runscript do
 end
 
 -- Redo combat check (R)
--- Heal entire group (M)
--- Target nearby enemies (enemies in combat) (M)
+-- Heal entire group (M) (Done)
+-- Target nearby enemies (enemies in combat) (M) (Done)
 	-- Debuff enemies
 	-- MQ Searching
 		-- next
@@ -273,4 +281,4 @@ end
 -- Navigation (R)
 
 -- Flowchart (M)
--- Video (R)s
+-- Video (R)
