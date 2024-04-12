2aee585e2992567b67b3d54409377ad645f6725f
Richard Jordan <jordanr1@sunypoly.edu>
2024-04-12 16:06:57 -0400

    added item summoning

--- Thalindor_Swiftblade.pl
+++ Thalindor_Swiftblade.pl
@@ -35,9 +35,13 @@ sub buy_return {
 my @buff_ids = (11, 18, 19, 20, 34, 66);
 my $test_buying = 0;
 
+sub EVENT_SPAWN {
+	$diamondhandin = 0;
+}
+
 sub EVENT_SAY {
 	if ($text=~/Hail/i) {
-		quest::say("Hello [Test_Buy] [Get Change] [Teleport]"); # Add to this as needed
+		quest::say("Hello [Test_Buy] [Get Change] [Teleport] [Summon Items]"); # Add to this as needed
 	}
 	elsif ($text=~/Teleport/i) {
 		quest::say("I am still learning about teleportation, but I can send you to [" . quest::saylink("Pillars of Alra") . "], [" . quest::saylink("Icefall Glacier") . "],  or [" . quest::saylink("Cobalt Scar") . "]. Where would you like to go?")
@@ -54,6 +58,20 @@ sub EVENT_SAY {
         quest::say("Cobalt Scar it is!");
         $npc->CastSpell(2025, $userid);
     }
+	elsif ($text =~ /Summon Items/i) {
+		quest::say("I can summon an item for you, please give me one Diamond and then tell me the id of the item you would like");
+	}
+	elsif ($text=~/(\d{1,6})/i) {
+		$itemNum = $text;
+		if ($diamondhandin) {
+			quest::say("Here is your item");
+			quest::summonitem($itemNum);
+			$diamondhandin = 0;
+		} else {
+			quest::say("I charge one Diamond for my services. Please hand me it and then ask again.");
+		}
+
+	}
 	elsif ($text=~/Leave/i) {
 		# Reset any variables about where the player is in the flowchart
 		$test_buying = 0;
@@ -69,25 +87,34 @@ sub EVENT_SAY {
 
 sub EVENT_ITEM {
 	my $money = $copper + 10*($silver + 10*($gold + 10*$platinum));
-	
-	
-	if ($test_buying) {
-		if (buy_return($money, 77)) {
-			quest::say("Thanks for the 77c");
-		}
-		else {
-			quest::say("Not enough, give me more or [Leave]");
-		}
+	my $item = $text;
+
+	# 10037 == diamond
+	if (plugin::check_handin(\%itemcount, 10037 => 1)) {
+		quest::say("Thank you for the diamond. Please tell me the ID of the item you want.");
+		$diamondhandin = 1;
 	}
+
+	# if ($test_buying) {
+	# 	if (buy_return($money, 77)) {
+	# 		quest::say("Thanks for the 77c");
+	# 	}
+	# 	else {
+	# 		quest::say("Not enough, give me more or [Leave]");
+	# 	}
+	# }
 	# elsif (...) {...}
-	else {
-		quest::say("Thanks for the tip, here's something for your generosity");
+	# else {
+	# 	quest::say("Thanks for the tip, here's something for your generosity");
 		
-		my $buff_index = int(rand(scalar @buff_ids));
-		quest::selfcast($buff_ids[$buff_index]);
+	# 	my $buff_index = int(rand(scalar @buff_ids));
+	# 	quest::selfcast($buff_ids[$buff_index]);
 		
-		if ($money >= 1000) {
-			give_change($money - 1000);
-		}
-	}
+	# 	if ($money >= 1000) {
+	# 		give_change($money - 1000);
+	# 	}
+	# }
+
+	#return any unused items
+	plugin::return_items(%itemcount);
 }
