2ab4a92e30aef9036031ec10796c3896e6522d77
Richard Jordan <jordanr1@sunypoly.edu>
2024-04-24 22:14:31 -0400

    fixed data bucket

--- Project/quests/Graldor_the_Gambler.pl
+++ Project/quests/Graldor_the_Gambler.pl
@@ -2,8 +2,15 @@
 # 
 sub EVENT_SAY { 
 if($text=~/Hail/i){
-
-quest::say("Ah, greetings adventurer. I see the winds of fate have brought you here. Perhaps you could assist me in a matter of [grave importance]"); 
+    my $bucket_key = $client->CharacterID() . "-the-gamblers-reckoning";
+    if(quest::get_data($bucket_key) == 2) {
+        quest::say("Thank you again for returning my item.[reset bucket]");
+    }
+    elsif(quest::get_data($bucket_key) == 1) {
+        quest::say("Have you found it yet? They should have it at the King's Court Casino. Please hurry![reset bucket]");
+    } else {
+        quest::say("Ah, greetings adventurer. I see the winds of fate have brought you here. Perhaps you could assist me in a matter of [grave importance]? [reset bucket]"); 
+    }
 }
 elsif ($text =~/grave importance/i) {
     quest::say(" I fear I've made a grievous error, one that may cost me dearly. You see, in a moment of reckless indulgence, I wagered an [item] of immense personal significance in a game of chance.");
@@ -14,7 +21,7 @@ elsif ($text =~/item/i) {
 elsif ($text =~/Where did you lose it?/i) {
     quest::say("At the King's Court Casino, a den of vice and temptation. I was drawn into a game of Dragon's Chance, and before I knew it, the pendant was on the table, lost in the shuffle of cards and coins. If you can find it, please return it to me.");
     my $bucket_key = $client->CharacterID() . "-the-gamblers-reckoning";
-    my $bucket_value = 70;
+    my $bucket_value = 1;
     quest::set_data($bucket_key, $bucket_value);
 
     if(quest::get_data($bucket_key)) {
@@ -24,18 +31,29 @@ elsif ($text =~/Where did you lose it?/i) {
         quest::say("Fail!")
     }
 }
+elsif ($text =~/reset bucket/i) {
+    my $bucket_key = $client->CharacterID() . "-the-gamblers-reckoning";
+    quest::set_data($bucket_key, 0);
+    quest::say("Bucket reset");
+}
 };
 
 sub EVENT_ITEM {
+    #todo change item to his quest item
     if (plugin::check_handin(\%itemcount, 10037 => 1)) {
-        if($client->GetBucket("the-gamblers-reckoning")) {
+        my $bucket_key = $client->CharacterID() . "-the-gamblers-reckoning";
+        if(quest::get_data($bucket_key) == 1) {
             quest::say("Success!");
-            # quest::delete_data($bucket_key);
-            # quest::say("success! bucket key is ($bucketvalue)");
+            #todo change reward to something more relevant
+            quest::summonitem(quest::ChooseRandom(13053, 10010, 10018, 10017));
+            quest::ding();
+            #status 2 for quest completed
+            quest::set_data($bucket_key, 2);
         }
         else {
              quest::say("Where did you get this?");
-           #  quest::say("fail! bucket key is ($bucketvalue)");
+            #return item
+             quest::summonitem(10037);
         }
 	}
 };
